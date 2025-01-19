ee5ddf740804444487c05febb524503a1f44a1e7
diana <dianaclabel08@gmail.com>
2025-01-18 19:10:31 -0500

    Se mejora las estadicticas de los libros

--- src/main/java/com/aluracursos/challengeBookSearch/principal/Principal.java
+++ src/main/java/com/aluracursos/challengeBookSearch/principal/Principal.java
@@ -5,10 +5,7 @@ import com.aluracursos.challengeBookSearch.model.Libro;
 import com.aluracursos.challengeBookSearch.services.ConsumoApi;
 import com.aluracursos.challengeBookSearch.services.TransformarDatos;
 
-import java.util.ArrayList;
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -21,6 +18,7 @@ public class Principal {
 
 
     public void interfaz(){
+        //Busqueda de libro por nombre
         System.out.println("Escribe el nombre del libro");
         var libro = keyboard.nextLine();
         System.out.println(URL_BASE+libro.replace(" ", "%20"));
@@ -31,11 +29,29 @@ public class Principal {
         System.out.println("--------------------------------------");
         System.out.println("Todas las coincidencias : " + infoLibros);
         System.out.println("--------------------------------------");
-        System.out.println("Te mostramos 5 coincidencias");
+        //System.out.println("Te mostramos 5 coincidencias");
 
+        /*
         infoLibros.libros().stream()
                 .limit(5)
                 .forEach(e -> System.out.println("Titulo: "+ e.titulo()+", Total de descarga: "+ e.totalDescargas()));
+
+         */
+
+        //El opcional me permite usar metodos como : isPresent entre otros, ademas de guardar el dato cmo una variable
+
+        Optional<Libro> libroBuscado = infoLibros.libros().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(libro.toUpperCase())).findFirst();
+
+        if (libroBuscado.isPresent()){
+            System.out.println("Libro encontardo:");
+            System.out.println(libroBuscado.get());
+        }else {
+            System.out.println("Libro no encontrado");
+        }
+
+
+
 
 
         //top de 10 libros mas descargados
@@ -46,16 +62,30 @@ public class Principal {
         String jsonTodo = api.obtenerDatosApi(URL_BASE);
         var todoLosLibros = conversorDeJson.conversor(jsonTodo, DatosLibro.class);
 
-        DoubleSummaryStatistics estadisticas = todoLosLibros.libros().stream()
+        todoLosLibros.libros().stream()
+                .sorted(Comparator.comparing(Libro::totalDescargas).reversed())
                 .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
+
+        System.out.println("--------------------------------------");
+
+        //TRABAJANDO CON ESTADISTICAS
+
+
+        DoubleSummaryStatistics estadisticas = todoLosLibros.libros().stream()
+                .filter(l -> l.totalDescargas() > 0)
                 .peek(e -> System.out.println(e))
                 .collect(Collectors.summarizingDouble(Libro::totalDescargas));
 
         System.out.println("--------------------------------------");
 
-        System.out.println("Libro con Mayor descarga: " + estadisticas.getMax());
-        System.out.println("Libro con Menor descarga: " + estadisticas.getMin());
-        System.out.println("Registros en total: "+ estadisticas.getCount());
+        System.out.println("cantidad maxima de descarga: " + estadisticas.getMax());
+        System.out.println("canntidad minima de descarga: " + estadisticas.getMin());
+        System.out.println("cantidad promedio de descarga: " + estadisticas.getAverage());
+        System.out.println("Cantidad de registros evaluados en total: "+ estadisticas.getCount());
+
+
 
 
 
